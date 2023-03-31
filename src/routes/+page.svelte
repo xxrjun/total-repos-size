fe33cc4137f62589591dc913ce7c721f6ad583e6
xxrjun <rjun0729@gmail.com>
2023-03-31 20:09:02 +0800

    Update About Page: Improved Content and Multilingual Support
    - Update about page with new title, description and instructions.
    - Fix indentation in one of the Svelte files.
    - Add a disclaimer indicating that app only calculates size of public repositories.
    - Add a new section in traditional Chinese explaining the purpose and usage of the app.
    
    [src/routes/+page.svelte]
    - Fix indentation: change tabs to spaces.
    [src/routes/about/+page.svelte]
    - Update the title and description of the page
    - Add a disclaimer about the app only calculating the size of public repositories
    - Add a paragraph explaining the purpose of the app and how to use it
    - Add a section in traditional Chinese explaining the purpose of the app and how to use it (for fun)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,224 +1,218 @@
 <script>
-  import { onMount } from "svelte";
-  import axios from "axios";
+	import { onMount } from 'svelte';
+	import axios from 'axios';
 
-  let userId1 = "";
-  let userId2 = "";
-  let totalRepos1 = 0;
-  let totalRepos2 = 0;
-  let totalSize1 = "";
-  let totalSize2 = "";
-  let compareResult = "";
+	let userId1 = '';
+	let userId2 = '';
+	let totalRepos1 = 0;
+	let totalRepos2 = 0;
+	let totalSize1 = '';
+	let totalSize2 = '';
+	let compareResult = '';
 
-  // Calculate the total size of all repositories
-  async function calculate() {
-    if (userId1) {
-      try {
-        const response = await axios.get(
-          `https://api.github.com/users/${userId1}/repos`
-        );
-        const repositories = response.data;
+	// Calculate the total size of all repositories
+	async function calculate() {
+		if (userId1) {
+			try {
+				const response = await axios.get(`https://api.github.com/users/${userId1}/repos`);
+				const repositories = response.data;
 
-        totalRepos1 = repositories.length;
-        const totalSize = repositories.reduce(
-          (/** @type {any} */ acc, /** @type {{ size: any; }} */ repo) =>
-            acc + repo.size,
-          0
-        );
+				totalRepos1 = repositories.length;
+				const totalSize = repositories.reduce(
+					(/** @type {any} */ acc, /** @type {{ size: any; }} */ repo) => acc + repo.size,
+					0
+				);
 
-        totalSize1 = formatSize(totalSize);
-      } catch (error) {
-        totalRepos1 = 0;
+				totalSize1 = formatSize(totalSize);
+			} catch (error) {
+				totalRepos1 = 0;
 
-        totalSize1 = "";
-        compareResult = "";
-        console.error(error);
-      }
-    } else {
-      totalRepos1 = 0;
-      totalSize1 = "";
-      compareResult = "";
-    }
+				totalSize1 = '';
+				compareResult = '';
+				console.error(error);
+			}
+		} else {
+			totalRepos1 = 0;
+			totalSize1 = '';
+			compareResult = '';
+		}
 
-    if (userId2) {
-      try {
-        const response = await axios.get(
-          `https://api.github.com/users/${userId2}/repos`
-        );
-        const repositories = response.data;
+		if (userId2) {
+			try {
+				const response = await axios.get(`https://api.github.com/users/${userId2}/repos`);
+				const repositories = response.data;
 
-        totalRepos2 = repositories.length;
-        const totalSize = repositories.reduce(
-          (/** @type {any} */ acc, /** @type {{ size: any; }} */ repo) =>
-            acc + repo.size,
-          0
-        );
-        totalSize2 = formatSize(totalSize);
-      } catch (error) {
-        totalRepos2 = 0;
-        totalSize2 = "";
-        compareResult = "";
-        console.error(error);
-      }
-    } else {
-      totalRepos2 = 0;
-      totalSize2 = "";
-      compareResult = "";
-    }
-  }
+				totalRepos2 = repositories.length;
+				const totalSize = repositories.reduce(
+					(/** @type {any} */ acc, /** @type {{ size: any; }} */ repo) => acc + repo.size,
+					0
+				);
+				totalSize2 = formatSize(totalSize);
+			} catch (error) {
+				totalRepos2 = 0;
+				totalSize2 = '';
+				compareResult = '';
+				console.error(error);
+			}
+		} else {
+			totalRepos2 = 0;
+			totalSize2 = '';
+			compareResult = '';
+		}
+	}
 
-  // Show total size in all kind of human-readable formats
-  /**
-   * @param {number} size
-   */
-  function formatSize(size) {
-    if (size < 1024 * 1024) {
-      return `${size.toFixed(2)} KB ( ${(size / 1024).toFixed(2)} MB)`;
-    } else if (size < 1024 * 1024 * 1024) {
-      return `${(size / 1024).toFixed(2)} MB ( ${(size / 1024 / 1024).toFixed(
-        2
-      )} GB)`;
-    } else {
-      return `${(size / 1024 / 1024).toFixed(2)} GB ( ${(
-        size /
-        1024 /
-        1024 /
-        1024
-      ).toFixed(2)} TB)`;
-    }
-  }
+	// Show total size in all kind of human-readable formats
+	/**
+	 * @param {number} size
+	 */
+	function formatSize(size) {
+		if (size < 1024 * 1024) {
+			return `${size.toFixed(2)} KB ( ${(size / 1024).toFixed(2)} MB)`;
+		} else if (size < 1024 * 1024 * 1024) {
+			return `${(size / 1024).toFixed(2)} MB ( ${(size / 1024 / 1024).toFixed(2)} GB)`;
+		} else {
+			return `${(size / 1024 / 1024).toFixed(2)} GB ( ${(size / 1024 / 1024 / 1024).toFixed(
+				2
+			)} TB)`;
+		}
+	}
 
-  onMount(() => {});
+	onMount(() => {});
 </script>
 
-<section class="container">
-  <h1 class="title">GitHub Repository Size Calculator</h1>
+<svelte:head>
+	<title>Home | GitHub Repository Size Calculator</title>
+	<meta name="description" content="GitHub Repository Size Calculator" />
+</svelte:head>
 
-  <form on:submit|preventDefault={calculate}>
-    <div class="grid">
-      <div class="card">
-        <label class="label" for="userId1">GitHub ID 1</label>
-        <input class="input" type="text" id="userId1" bind:value={userId1} />
-        <button class="button" on:click={calculate}>Calculate</button>
-        {#if totalRepos1 !== null}
-          <div class="result">
-            <p>
-              User <span class="username">{userId1}</span> has
-              <span class="repos">{totalRepos1}</span>
-              public repositories, and the total size is
-              <span class="size">{totalSize1}</span>
-            </p>
-          </div>
-        {/if}
-      </div>
-      <div class="card">
-        <label class="label" for="userId2">GitHub ID 2</label>
-        <input class="input" type="text" id="userId2" bind:value={userId2} />
-        <button class="button" on:click={calculate}>Calculate</button>
-        {#if totalRepos2 !== null}
-          <div class="result">
-            <p>
-              User <span class="username">{userId2}</span> has
-              <span class="repos">{totalRepos2}</span>
-              public repositories, and the total size is
-              <span class="size">{totalSize2}</span>
-            </p>
-          </div>
-        {/if}
-      </div>
-    </div>
-  </form>
+<section class="container">
+	<h1 class="title">GitHub Repository Size Calculator</h1>
+
+	<form on:submit|preventDefault={calculate}>
+		<div class="grid">
+			<div class="card">
+				<label class="label" for="userId1">GitHub ID 1</label>
+				<input class="input" type="text" id="userId1" bind:value={userId1} />
+				<button class="button" on:click={calculate}>Calculate</button>
+				{#if totalRepos1 !== null}
+					<div class="result">
+						<p>
+							User <span class="username">{userId1}</span> has
+							<span class="repos">{totalRepos1}</span>
+							public repositories, and the total size is
+							<span class="size">{totalSize1}</span>
+						</p>
+					</div>
+				{/if}
+			</div>
+			<div class="card">
+				<label class="label" for="userId2">GitHub ID 2</label>
+				<input class="input" type="text" id="userId2" bind:value={userId2} />
+				<button class="button" on:click={calculate}>Calculate</button>
+				{#if totalRepos2 !== null}
+					<div class="result">
+						<p>
+							User <span class="username">{userId2}</span> has
+							<span class="repos">{totalRepos2}</span>
+							public repositories, and the total size is
+							<span class="size">{totalSize2}</span>
+						</p>
+					</div>
+				{/if}
+			</div>
+		</div>
+	</form>
 </section>
 
 <style>
-  .container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 2rem;
-  }
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 2rem;
+	}
 
-  .title {
-    font-size: 2rem;
-    font-weight: bold;
-    margin-bottom: 2rem;
-  }
+	.title {
+		font-size: 2rem;
+		font-weight: bold;
+		margin-bottom: 2rem;
+	}
 
-  form {
-    width: 100%;
-  }
+	form {
+		width: 100%;
+	}
 
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-    grid-gap: 2rem;
-  }
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+		grid-gap: 2rem;
+	}
 
-  .card {
-    border-radius: 0.5rem;
-    padding: 1rem;
-    background-color: #f7f7f7;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
+	.card {
+		border-radius: 0.5rem;
+		padding: 1rem;
+		background-color: #f7f7f7;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	}
 
-  .label {
-    display: block;
-    font-size: 1.2rem;
-    font-weight: bold;
-    margin-bottom: 0.5rem;
-  }
+	.label {
+		display: block;
+		font-size: 1.2rem;
+		font-weight: bold;
+		margin-bottom: 0.5rem;
+	}
 
-  .input {
-    padding: 0.5rem;
-    border-radius: 0.5rem;
-    border: none;
-    background-color: #fff;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    width: 90%;
-    margin-bottom: 1rem;
-  }
+	.input {
+		padding: 0.5rem;
+		border-radius: 0.5rem;
+		border: none;
+		background-color: #fff;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+		width: 90%;
+		margin-bottom: 1rem;
+	}
 
-  .button {
-    padding: 0.5rem 1rem;
-    border-radius: 0.5rem;
-    border: none;
-    background-color: #007bff;
-    color: #fff;
-    font-weight: bold;
-    cursor: pointer;
-    transition: background-color 0.2s ease;
-  }
+	.button {
+		padding: 0.5rem 1rem;
+		border-radius: 0.5rem;
+		border: none;
+		background-color: #007bff;
+		color: #fff;
+		font-weight: bold;
+		cursor: pointer;
+		transition: background-color 0.2s ease;
+	}
 
-  .button:hover {
-    background-color: #0062cc;
-  }
+	.button:hover {
+		background-color: #0062cc;
+	}
 
-  .result {
-    margin-top: 1rem;
-  }
+	.result {
+		margin-top: 1rem;
+	}
 
-  .result p {
-    font-size: 1.2rem;
-    line-height: 1.5;
-  }
+	.result p {
+		font-size: 1.2rem;
+		line-height: 1.5;
+	}
 
-  .username {
-    font-weight: bold;
-  }
+	.username {
+		font-weight: bold;
+	}
 
-  .repos {
-    font-weight: bold;
-    margin: 0 0.2rem;
-  }
+	.repos {
+		font-weight: bold;
+		margin: 0 0.2rem;
+	}
 
-  .size {
-    font-weight: bold;
-    margin: 0 0.2rem;
-  }
+	.size {
+		font-weight: bold;
+		margin: 0 0.2rem;
+	}
 
-  @media screen and (max-width: 768px) {
-    .grid {
-      grid-template-columns: 1fr;
-    }
-  }
+	@media screen and (max-width: 768px) {
+		.grid {
+			grid-template-columns: 1fr;
+		}
+	}
 </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,21 +1,38 @@
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+	<title>About | GitHub Repository Size Calculator</title>
+	<meta name="description" content="About GitHub Repository Size Calculator" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>About GitHub Repository Size Calculator</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		This is a GitHub Repository Size Calculator app built with <a href="https://kit.svelte.dev"
+			>SvelteKit</a
+		>.
 	</p>
 
-	<pre>npm create svelte@latest</pre>
+	<p>
+		By simply entering your GitHub ID, the app will display the total number and combined size of
+		all your public repositories on GitHub.
+	</p>
 
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		Please note that this app only calculates the size of public repositories, and the size shown is
+		the compressed size, which may differ from the actual size after downloading (cloning) the
+		repositories.
+	</p>
+
+	<hr />
+
+	<p>
+		這是一個使用 <a href="https://kit.svelte.dev">SvelteKit</a> 構建的 GitHub 存儲庫大小計算器應用程式。
+		Just for FUN
+	</p>
+
+	<p>只要輸入 GitHub ID，就可以顯示所有你在 GitHub 上所有公開存儲庫的個數與總大小。</p>
+
+	<p>
+		請注意這個應用程式只計算公開存儲庫的大小。而且這個大小應該是經過壓縮的，因此不會等同下載(Clone)後的大小。
 	</p>
 </div>
